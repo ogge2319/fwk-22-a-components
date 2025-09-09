eab11cf5dad7e80613bc55fc53a2d3c433287848
Ali Taei <aalitaei@gmail.com>
2025-09-09 11:32:00 +0200

    export fix

---
+++ src/components/Button/index.js
@@ -0,0 +1,8 @@
+export { default as Button } from "./Button/Button";
+export { default as Header } from "./Header/Header";
+export { default as HomePage } from "./HomePage/HomePage";
+export { default as Cell } from "./Cell/Cell";
+export { default as Controls } from "./Controls/Controls";
+export { default as GameStatus } from "./GameStatus/GameStatus";
+export { default as ScoreBoard } from "./ScoreBoard/ScoreBoard";
+export { default as Board } from "./Board/Board";
