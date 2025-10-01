edfd9423d0522af9caaa0e6c4e932bf5373af62a
Hodman Liban <Hodman_libanh@hotmail.com>
2025-10-01 19:08:35 +0200

    feat: add flexible Button component with onClick and onStartGame props

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,39 @@
-export default function Button() {
-    return <button>Klicka här</button>;
-  }
+import PropTypes from 'prop-types';
+import styles from './Button.module.css';
+
+export default function Button({ 
+    children = "Klicka här", 
+    onClick, 
+    onStartGame, 
+    disabled = false,
+    type = "button",
+    ...props 
+}) {
+    const handleClick = (e) => {
+        if (disabled) return;
+        
+        onClick?.(e);
+        onStartGame?.(e);
+    };
+
+    return (
+        <button 
+            className={styles.button}
+            onClick={handleClick}
+            disabled={disabled}
+            type={type}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+}
+
+Button.propTypes = {
+    children: PropTypes.node,
+    onClick: PropTypes.func,
+    onStartGame: PropTypes.func,
+    disabled: PropTypes.bool,
+    type: PropTypes.oneOf(['button', 'submit', 'reset']),
+};
   
