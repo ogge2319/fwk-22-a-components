f6168831cbf802f005a6f78e80e7a3a6336f46ab
Hodman Liban <Hodman_libanh@hotmail.com>
2025-10-01 23:06:27 +0200

    Fix button clickability issues

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,5 @@ export { default as Controls } from "./Controls/Controls.jsx";
 export { default as ScoreBoard } from "./ScoreBoard/ScoreBoard.jsx";
 export { default as GameStatus } from "./GameStatus/GameStatus.jsx";
 export { default as HomePage } from "./HomePage/HomePage.jsx";
+export { default as Modal } from "./Modal/Modal.jsx";
+export { default as AboutModal } from "./AboutModal/AboutModal.jsx";
